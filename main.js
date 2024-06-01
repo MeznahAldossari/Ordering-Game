5d2d488dd39cc1111ca2a2f175b79e0c8aaebcc2
MeznahAldossari <MeznahAldossari@hotmail.com>
2024-06-01 17:48:13 +0300

    v2

--- main.js
+++ main.js
@@ -22,10 +22,17 @@ function resizeWindow() {
 }
 
 let context = canvas1.getContext("2d");
+let arrImages = [];
+let details = [];
+
+imagesList.forEach((image) => {
+  for (let x = 0; x < 3; x++) {
+    arrImages.push(image);
+  }
+});
+arrImages.sort(() => Math.random() - 0.5);
 
 function Box() {
-  //   context.fillStyle = "#B67352";
-  //   context.fillRect(250, 30, 810, 460);
   context.clearRect(0, 0, canvas1.width, canvas1.height);
 
   let mainBox = context.createLinearGradient(250, 50, 250, 30 + 460);
@@ -42,44 +49,27 @@ function Box() {
 
   let vertically = 70;
   let horizontally;
-  let x;
-  let y;
-
-  imagesObj = {};
-  arrImages = [];
-  imagesList.forEach((image) => {
-    for (let x = 0; x < 3; x++) {
-      arrImages.push(image);
-    }
-  });
-  arrImages.sort(() => Math.random() - 0.5);
 
   for (let x = 0; x < 3; x++) {
     horizontally = 298;
 
     for (let y = 0; y < 5; y++) {
       BoxColors(horizontally, vertically, "#EAD8C0", "rgba(255,255,255,0.4)");
-      //   boxDetails(horizontally, vertically);
-      //   horizontally = 430;
-
-      let imgHorizontal = horizontally;
-      let imgvertical = vertically + 30;
 
       for (let i = 0; i < 3; i++) {
         let randomImage = arrImages.pop();
-        // let image = new Image();
-        // image.src = `./images/${randomImage}`;
-
-        // if (imagesObj[randomImage] && imagesObj[randomImage] >= 3) {
-        //   randomImage = ImagesRandom();
-        //   console.log("hello");
-        // }
-
         let imageHorizon = horizontally + 5 + i * (30 + 5);
         let imageVerticl = vertically + 30 + 5;
 
-        drawingImage(randomImage, imageHorizon, imageVerticl, 60, 80);
-        imagesObj[randomImage] = (imagesObj[randomImage] || 0) + 1;
+        let detail = {
+          names: randomImage,
+          x: imageHorizon,
+          y: imageVerticl,
+          w: 60,
+          h: 80,
+        };
+        details.push(detail);
+        drawingImage(detail);
       }
 
       horizontally += 120 + 25;
@@ -87,11 +77,12 @@ function Box() {
 
     vertically += 120 + 25;
   }
+
+  details.forEach((detail) => {
+    drawingImage(detail);
+  });
 }
 
-function ImagesRandom() {
-  return imagesList[Math.floor(Math.random() * imagesList.length)];
-}
 function BoxColors(h, v, color, gloss) {
   let firstColor = context.createLinearGradient(h, v, h, v + 120);
   firstColor.addColorStop(0, color);
@@ -108,13 +99,14 @@ function BoxColors(h, v, color, gloss) {
   context.fillRect(h, v, 130, 120);
 }
 
-function drawingImage(src, x, y, w, h) {
+function drawingImage(detail) {
   let image = new Image();
-  image.src = `./images/${src}`;
+  image.src = `./images/${detail.names}`;
   image.onload = () => {
-    context.drawImage(image, x, y, w, h);
+    context.drawImage(image, detail.x, detail.y, detail.w, detail.h);
   };
 }
+
 resizeWindow();
 Box();
 
@@ -122,11 +114,78 @@ window.addEventListener("resize", () => {
   resizeWindow();
   Box();
 });
-addEventListener("mousemove", (e) => {
-  handImage.style.cssText = `
-        top:${
-          e.clientY - canvas1.offsetTop - handImage.offsetHeight / 2 + 60
-        }px;
-        left:${e.clientX - canvas1.offsetLeft - handImage.offsetWidth / 2}px;
-    `;
+addEventListener("mousemove", (event) => {
+  mx = event.clientX;
+  my = event.clientY;
+
+  // Update the position of the hand image, keeping the cursor at the top
+  handImage.style.left = `${mx + 77}px`;
+  handImage.style.top = `${my - 35}px`;
 });
+
+let XAxis;
+let YAxis;
+let startX, startY;
+let dragging = false;
+let current;
+
+let inImage = function (x, y, image) {
+  let imageLeft = image.x;
+  let imageRight = image.x + image.w;
+
+  let imageTop = image.y;
+  let imagebottom = image.y + image.h;
+
+  return x > imageLeft && x < imageRight && y > imageTop && y < imagebottom;
+};
+
+let handDown = function (event) {
+  event.preventDefault();
+  XAxis = parseInt(event.clientX);
+  YAxis = parseInt(event.clientY);
+
+  for (let i = 0; i < details.length; i++) {
+    if (inImage(XAxis, YAxis, details[i])) {
+      current = i;
+      dragging = true;
+      startX = XAxis;
+      startY = YAxis;
+
+      return;
+    }
+  }
+};
+
+let handUp = function (event) {
+  event.preventDefault();
+  if (!dragging) return;
+  dragging = false;
+};
+
+let handOut = function (event) {
+  event.preventDefault();
+  if (!dragging) return;
+  dragging = false;
+};
+
+let handMove = function (event) {
+  if (!dragging) return;
+  event.preventDefault();
+
+  let handX = parseInt(event.clientX);
+  let handY = parseInt(event.clientY);
+
+  let dx = handX - startX;
+  let dy = handY - startY;
+  let currenImage = details[current];
+  currenImage.x += dx;
+  currenImage.y += dy;
+  startX = handX;
+  startY = handY;
+  Box();
+};
+
+document.addEventListener("mousedown", handDown);
+document.addEventListener("mouseup", handUp);
+document.addEventListener("mouseout", handOut);
+document.addEventListener("mousemove", handMove);
